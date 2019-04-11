c0937d39a658e96b65b4f5fb1a2d2531ccd13c85
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-04-11 17:33:01 +0300

    animation prepare

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,100 +1,22 @@
 import Vue from "vue";
 
-const compA = {
-  template: "#comp-a"
-};
-
-const compB = {
-  template: "#comp-b"
-};
-
-const customInput = {
-  template: "#input",
-  props: ['value']
-}
-
-const animationTest = {
-  components: {
-    compA,
-    compB,
-    customInput
-  },
-  template: "#test",
-  data() {
-    return {
-      compToShow: "compA",
-      model: ""
-    };
-  },
-  methods: {
-    add() {
-      this.arr.shift();
-      this.arr.push(this.arr[this.arr.length - 1] + 1);
-    },
-    change() {
-      this.compToShow = this.compToShow === 'compA' ? 'compB' : 'compA'
-    }
-  }
-};
-
 new Vue({
-  components: {
-    animationTest
-  },
   el: "#animations-container",
   template: "#animation",
   data() {
     return {
       works: [],
-      currentIndex: 0,
-      animationName: "slideUp"
+      currentIndex: 0
     };
   },
-  computed: {
-    filteredArray() {
-      return [...this.works].splice(0, 3);
-    }
-  },
   methods: {
-    enterCb(el, done) {
-      const list = el.closest("ul");
-
-      el.classList.add("outside");
-      list.style.top = "100px";
-      list.classList.add("transition");
-
-      list.addEventListener("transitionend", e => {
-        done();
-      });
-    },
-
-    leaveCb(el, done) {
-      el.classList.remove("fade");
-      el.classList.add("fade");
-    },
-
-    afterCb(el) {
-      const list = el.closest("ul");
-      list.classList.remove("transition");
-      list.style.top = 0;
-      el.classList.remove("outside");
-    },
     slide(direction) {
-      // const worksArray = [...this.works];
-
       switch (direction) {
         case "up":
-          this.works.push(this.works[0]);
-          this.works.shift();
           this.currentIndex++;
-          this.animationName = "slideUp";
           break;
         case "down":
-          const lastItem = this.works[this.works.length - 1];
-          this.works.unshift(lastItem);
-          this.works.pop();
           this.currentIndex--;
-          this.animationName = "slideDown";
           break;
       }
     },
