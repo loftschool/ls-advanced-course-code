3037e8b2bafb82d97a449fd4a0fb20ad50b8e29d
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-07-19 12:13:36 +0300

    вынес группу из скиллов

--- src/admin/components/pages/about.vue
+++ src/admin/components/pages/about.vue
@@ -10,26 +10,16 @@
       .container.container--mobile-wide
         ul.skill-list
           li.skill-list__item
-            .skill-container
-              input(type="text" placeholder="Создать группу").skill-input
-              button(type="button") Добавить
-              hr
-              table
-                tr(v-for="n in 10")
-                  td Скилл
-                  td Процент
-                  td 
-                    button(type="button") удалить
-                    button(type="button") изменить
-              hr
-              .add-skill-wrapper.blocked
-                input(type="text" placeholder="Добавить скилл")
-                button(type="button") Добавить
+            skills-group()
 
 </template>
 
 <script>
-export default {};
+export default {
+  components: {
+    skillsGroup: () => import("components/skills-group.vue")
+  }
+};
 </script>
 
 <style lang="postcss" scoped>
---
+++ src/admin/components/skills-group.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .skill-container
+    input(type="text" placeholder="Создать группу").skill-input
+    button(type="button") Добавить
+    hr
+    table
+      tr(v-for="n in 10")
+        td Скилл
+        td Процент
+        td 
+          button(type="button") удалить
+          button(type="button") изменить
+    hr
+    .add-skill-wrapper.blocked
+      input(type="text" placeholder="Добавить скилл")
+      button(type="button") Добавить
+</template>
+<script>
+export default {
+  
+}
+</script>
+
