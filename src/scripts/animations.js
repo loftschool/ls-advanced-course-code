a2890ee111e2939df64903e7c2fe56bedfeaefb2
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-07-26 21:21:46 +0300

    ready

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -5,18 +5,52 @@ new Vue({
   template: "#animation",
   data() {
     return {
-      works: [],
-      currentIndex: 0
+      works: []
     };
   },
+  computed: {
+    splicedWorks() {
+      return [...this.works].splice(0, 3);
+    }
+  },
   methods: {
+    enterCb(el, done) {
+      const list = el.closest("ul");
+
+      el.classList.add("outsided");
+      list.style.top = "100px";
+
+      list.classList.add("transition");
+
+      list.addEventListener("transitionend", e => {
+        done();
+      });
+    },
+    
+    afterCb(el) {
+      const list = el.closest("ul");
+
+      el.classList.remove("outsided");
+      list.classList.remove("transition");
+
+      list.style.top = 0;
+    },
+
+    leaveCb(el) {
+      // el.classList.remove("fade");
+      el.classList.add("fade");
+    },
+
     slide(direction) {
       switch (direction) {
         case "up":
-          this.currentIndex++;
+          this.works.push(this.works[0]);
+          this.works.shift();
           break;
         case "down":
-          this.currentIndex--;
+          const lastItem = this.works[this.works.length - 1];
+          this.works.unshift(lastItem);
+          this.works.pop();
           break;
       }
     },
@@ -27,17 +61,6 @@ new Vue({
 
         return item;
       });
-    },
-    makeInfititeLoopForCurIndex(value) {
-      const worksAmount = this.works.length - 1;
-
-      if (value > worksAmount) this.currentIndex = 0;
-      if (value < 0) this.currentIndex = worksAmount;
-    }
-  },
-  watch: {
-    currentIndex(value) {
-      this.makeInfititeLoopForCurIndex(value);
     }
   },
   created() {
