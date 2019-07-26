b311a26459775c790c79e2c962bc83b12120d4c3
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-07-26 21:30:20 +0300

    убрал лишний код

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,53 +1,11 @@
 import Vue from "vue";
 
-const compA = {
-  template: "#comp-a"
-};
-
-const compB = {
-  template: "#comp-b"
-};
-
-const customInput = {
-  template: "#input",
-  props: ['value']
-}
-
-const animationTest = {
-  components: {
-    compA,
-    compB,
-    customInput
-  },
-  template: "#test",
-  data() {
-    return {
-      compToShow: "compA",
-      model: ""
-    };
-  },
-  methods: {
-    add() {
-      this.arr.shift();
-      this.arr.push(this.arr[this.arr.length - 1] + 1);
-    },
-    change() {
-      this.compToShow = this.compToShow === 'compA' ? 'compB' : 'compA'
-    }
-  }
-};
-
 new Vue({
-  components: {
-    animationTest
-  },
   el: "#animations-container",
   template: "#animation",
   data() {
     return {
       works: [],
-      currentIndex: 0,
-      animationName: "slideUp"
     };
   },
   computed: {
@@ -80,21 +38,15 @@ new Vue({
       el.classList.remove("outside");
     },
     slide(direction) {
-      // const worksArray = [...this.works];
-
       switch (direction) {
         case "up":
           this.works.push(this.works[0]);
           this.works.shift();
-          this.currentIndex++;
-          this.animationName = "slideUp";
           break;
         case "down":
           const lastItem = this.works[this.works.length - 1];
           this.works.unshift(lastItem);
           this.works.pop();
-          this.currentIndex--;
-          this.animationName = "slideDown";
           break;
       }
     },
@@ -105,12 +57,6 @@ new Vue({
 
         return item;
       });
-    },
-    makeInfititeLoopForCurIndex(value) {
-      const worksAmount = this.works.length - 1;
-
-      if (value > worksAmount) this.currentIndex = 0;
-      if (value < 0) this.currentIndex = worksAmount;
     }
   },
   watch: {
