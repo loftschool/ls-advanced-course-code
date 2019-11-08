99d13fd1f99e0b83bc209d3e5bcf9fbd7c47b76a
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2019-11-08 18:28:41 +0300

    убрали лишний код

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -8,49 +8,12 @@ new Vue({
       works: []
     };
   },
-  computed: {
-    splicedWorks() {
-      return [...this.works].splice(0, 3);
-    }
-  },
   methods: {
-    enterCb(el, done) {
-      const list = el.closest("ul");
-
-      el.classList.add("outsided");
-      list.style.top = "100px";
-
-      list.classList.add("transition");
-
-      list.addEventListener("transitionend", e => {
-        done();
-      });
-    },
-    
-    afterCb(el) {
-      const list = el.closest("ul");
-
-      el.classList.remove("outsided");
-      list.classList.remove("transition");
-
-      list.style.top = 0;
-    },
-
-    leaveCb(el) {
-      // el.classList.remove("fade");
-      el.classList.add("fade");
-    },
-
     slide(direction) {
       switch (direction) {
         case "up":
-          this.works.push(this.works[0]);
-          this.works.shift();
           break;
         case "down":
-          const lastItem = this.works[this.works.length - 1];
-          this.works.unshift(lastItem);
-          this.works.pop();
           break;
       }
     },
